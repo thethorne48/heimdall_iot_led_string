db89181ec3e358c662695676bcc7806ff0c615e1
Luke Thorne <the.thorne48@gmail.com>
2019-09-25 00:02:43 -0400

    bunch of stuff

--- checkConfig.js
+++ checkConfig.js
@@ -1,19 +1,31 @@
 const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-const readline = util.promisify(require('readline')
-	.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	}))
+const exec = util.promisify(require('child_process').exec)
+const rl = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
 
-module.exports = (config) => {
+const question = (input) => {
+	return new Promise((resolve) => {
+		rl.question(input, answer => {
+			resolve(answer)
+			rl.close()
+		})
+	})
+}
+
+async function checkConfig(config) {
 	if (!config.HOST_URL) {
-		config.HOST_URL = await readline('Please enter a valid AWS Host URL:')
-		exec(`snapctl set host-url ${config.HOST_URL}`)
+		config.HOST_URL = await question('Please enter a valid AWS Host URL: ')
+		await exec(`snapctl set host-url ${config.HOST_URL}`)
 	}
 	if (!config.CERTIFICATE_ID) {
-		config.CERTIFICATE_ID = await readline('Please enter a valid AWS Certificate ID:')
-		exec(`snapctl set certificate-id ${config.CERTIFICATE_ID}`)
+		config.CERTIFICATE_ID = question('Please enter a valid AWS Certificate ID: ')
+		await exec(`snapctl set certificate-id ${config.CERTIFICATE_ID}`)
 	}
 	return config
+}
+
+module.exports = {
+	checkConfig
 }
--- heimdall-ledstring.js
+++ heimdall-ledstring.js
@@ -1,74 +1,79 @@
 const resolve = require('path').resolve
-
-const checkConfig = require('./checkConfig')
-const config = checkConfig(require(process.env.OPTS_JSON))
+const checkConfig = require('./checkConfig').checkConfig
 
 const ThingShadow = require('aws-iot-device-sdk').thingShadow
 // const Device = require('aws-iot-device-sdk').device
 // const Jobs = require('aws-iot-device-sdk').jobs
 
-const HOST_URL = config.HOST_URL
-const CLIENT_ID = config.CLIENT_ID
-const CA_FILE_NAME = config.CA_FILE_NAME
-const CERTIFICATE_ID = config.CERTIFICATE_ID
-const CERTIFICATE_PATH = resolve(`${process.env.SNAP_USER_DATA}/${config.CERTIFICATE_PATH}`)
-const CERTIFICATE_PREFIX = resolve(`${CERTIFICATE_PATH}/${CERTIFICATE_ID}`)
+async function main() {
+  // const config = checkConfig(require(process.env.OPTS_JSON)).then(config => config)
+  const config = await checkConfig({})
 
-const shadow = new ThingShadow({
-  keyPath: `${CERTIFICATE_PREFIX}-private.key.pem`,
-  certPath: `${CERTIFICATE_PREFIX}-certificate.pem.crt`,
-  caPath: `${CERTIFICATE_PATH}/${CA_FILE_NAME}`,
-  clientId: CLIENT_ID,
-  host: HOST_URL,
-  offlineQueueMaxSize: 50,
-  offlineQueueDropBehavior: 'oldest',
-})
+  const HOST_URL = config.HOST_URL
+  const CLIENT_ID = config.CLIENT_ID
+  const CA_FILE_NAME = config.CA_FILE_NAME
+  const CERTIFICATE_ID = config.CERTIFICATE_ID
+  const CERTIFICATE_PATH = resolve(`${process.env.SNAP_USER_DATA}/${config.CERTIFICATE_PATH}`)
+  const CERTIFICATE_PREFIX = resolve(`${CERTIFICATE_PATH}/${CERTIFICATE_ID}`)
 
-shadow.on('connect', () => {
-  shadow.register(thingName, {}, () => {
-    const state = {
-      state: {
-        desired: {
-          foo: 'bar',
-          ledCount: 150,
-          brightnessPercent: 80,
+  const shadow = new ThingShadow({
+    keyPath: `${CERTIFICATE_PREFIX}-private.key.pem`,
+    certPath: `${CERTIFICATE_PREFIX}-certificate.pem.crt`,
+    caPath: `${CERTIFICATE_PATH}/${CA_FILE_NAME}`,
+    clientId: CLIENT_ID,
+    host: HOST_URL,
+    offlineQueueMaxSize: 50,
+    offlineQueueDropBehavior: 'oldest',
+  })
+
+  shadow.on('connect', () => {
+    shadow.register(thingName, {}, () => {
+      const state = {
+        state: {
+          desired: {
+            foo: 'bar',
+            ledCount: 150,
+            brightnessPercent: 80,
+          },
         },
-      },
-    }
+      }
 
-    const tokenUpdate = shadow.update(thingName, state)
-    if (tokenUpdate === null) {
-      console.log('update shadow failed, operation still in progress')
-    } else {
-      console.log(`Token received: ${tokenUpdate}`)
-    }
+      const tokenUpdate = shadow.update(thingName, state)
+      if (tokenUpdate === null) {
+        console.log('update shadow failed, operation still in progress')
+      } else {
+        console.log(`Token received: ${tokenUpdate}`)
+      }
 
-    shadow.publish('test_topic', JSON.stringify({ from: thingName }), { qos: 0 }, () => {
-      console.log(`publish successful`)
+      shadow.publish('test_topic', JSON.stringify({ from: thingName }), { qos: 0 }, () => {
+        console.log(`publish successful`)
+      })
     })
   })
-})
 
-shadow.on('status', (thingName, stat, clientToken, stateObject) => {
-  console.log(`received ${stat} (token: ${clientToken}) on ${thingName}: ${JSON.stringify(stateObject)}`)
-})
+  shadow.on('status', (thingName, stat, clientToken, stateObject) => {
+    console.log(`received ${stat} (token: ${clientToken}) on ${thingName}: ${JSON.stringify(stateObject)}`)
+  })
 
-shadow.on('delta', (thing, state) => {
-  console.log(`Received delta on ${thing}: ${JSON.stringify(state)}`)
-})
+  shadow.on('delta', (thing, state) => {
+    console.log(`Received delta on ${thing}: ${JSON.stringify(state)}`)
+  })
 
-shadow.on('timeout', (thing, clientToken) => {
-  console.log(`Received timeout on ${thing} with token: ${clientToken}`)
-})
+  shadow.on('timeout', (thing, clientToken) => {
+    console.log(`Received timeout on ${thing} with token: ${clientToken}`)
+  })
 
-shadow.subscribe(`home/things/${thingName}/led/set`, { qos: 0 }, (err, granted) => {
-  if (err) {
-    console.log(err.message)
-  }
-  console.log(`Subscribed to ${granted[0].topic} with QoS: ${granted[0].qos}`)
-})
+  shadow.subscribe(`home/things/${thingName}/led/set`, { qos: 0 }, (err, granted) => {
+    if (err) {
+      console.log(err.message)
+    }
+    console.log(`Subscribed to ${granted[0].topic} with QoS: ${granted[0].qos}`)
+  })
 
-shadow.on('message', (topic, payload) => {
-  const json = JSON.parse(payload.toString('utf8'))
-  console.log(`Received message on ${topic}: ${JSON.stringify(json)}`)
-})
+  shadow.on('message', (topic, payload) => {
+    const json = JSON.parse(payload.toString('utf8'))
+    console.log(`Received message on ${topic}: ${JSON.stringify(json)}`)
+  })
+}
+
+main()
