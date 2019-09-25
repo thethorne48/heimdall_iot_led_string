142d67158cb117c7e27cb8fd75da2e471297798b
Luke Thorne <thethorne48@users.noreply.github.com>
2019-09-25 01:33:31 -0400

    Feature/snapcraft (#1)
    * update github actions yaml
    
    * modify things
    
    * update refs syntax
    
    * _ is invalid in snapctl
    
    * idk if this will work
    
    * remove some stuff
    
    * maybe this works
    
    * new config
    
    * mkdir -p
    
    * idk man
    
    * update config
    
    * change permissions
    
    * who really needs a config hook anyway
    
    * bunch of stuff
    
    * pls
    
    * it works locally now so that is nice
    
    * who knows
    
    * add stage-packages
    
    * add node command
    
    * extending try logic
    
    * no more try
    
    * minor changes
    
    * disable workflows for now
    
    * buildon armhf
    
    * who needs stage packages

---
+++ checkConfig.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec)
+const rl = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
+
+const question = (input) => {
+	return new Promise((resolve) => {
+		rl.question(input, answer => {
+			rl.close()
+			resolve(answer)
+		})
+	})
+}
+
+async function checkConfig(config) {
+	if (!config.HOST_URL) {
+		config.HOST_URL = await question('Please enter a valid AWS Host URL: ')
+		// await exec(`snapctl set host-url ${config.HOST_URL}`)
+		console.log(`config received: ${JSON.stringify(config)}`)
+	}
+	if (!config.CERTIFICATE_ID) {
+		config.CERTIFICATE_ID = await question('Please enter a valid AWS Certificate ID: ')
+		// await exec(`snapctl set certificate-id ${config.CERTIFICATE_ID}`)
+		console.log(`config received: ${JSON.stringify(config)}`)
+	}
+	console.log(`config received: ${JSON.stringify(config)}`)
+	return config
+}
+
+module.exports = {
+	checkConfig
+}
